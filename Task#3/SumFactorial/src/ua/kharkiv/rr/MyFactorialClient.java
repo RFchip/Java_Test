c6141a8ed63e34da6d6fc13047e694389316cb16
Ruslan <raietskiy@gmail.com>
2015-06-22 23:57:05 +0300

    Added comments to SumFactorial

--- Task#3/SumFactorial/src/ua/kharkiv/rr/MyFactorialClient.java
+++ Task#3/SumFactorial/src/ua/kharkiv/rr/MyFactorialClient.java
@@ -1,19 +1,35 @@
+/*
+ * %W% %E% Raietskyi Ruslan
+ * This software is the client for computing sum 
+ * of the digits, which represent factorial of incoming value.
+ */
 package ua.kharkiv.rr;
 
 import java.util.Scanner;
 
+/**
+ * Class MyFactorialClient - client for computing sum 
+ * of the digits, which represent factorial of incoming value.
+ * 
+ * @version 1.0 22 June 2015
+ * @author Raietskyi Ruslan
+ */
 public class MyFactorialClient {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		while (true) {
+			/* An invitation to enter the value */
 			System.out.println("Input value for factorial computing");
 			if (sc.hasNextInt()) {
+				
+				/* Checking argument, if number - computing sum. */
 				System.out.println("Answer: " + MyFactorial.factorialSum(sc.nextInt()));
 				sc.close();
 				System.exit(1);
 			} else {
+				/* Checking argument - if not number. */
 				System.err.println("Wrong type of argument! Try again!");
 				sc.nextLine();
 			}
